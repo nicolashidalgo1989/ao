c82ed8b6f813e53465c8e255b6c8e875dc306883
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2020-02-27 21:12:18 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+$('.owl-carousel.productos').owlCarousel({
+    margin: 0,
+    responsiveClass: true,
+    autoWidth: true,
+    responsive: {
+        0: {
+            items: 2,
+            mouseDrag: true,
+            touchDrag: true
+        },
+        600: {
+            items: 2,
+            mouseDrag: false,
+            touchDrag: false
+        },
+        1000: {
+            items: 2,
+            mouseDrag: false,
+            touchDrag: false
+        }
+    }
+});
+
+$('.owl-carousel.aceites').owlCarousel({
+    margin: 5,
+    responsiveClass: true,
+    autoWidth: true,
+    responsive: {
+        0: {
+            items: 2,
+            margin: 30,
+            mouseDrag: true,
+            touchDrag: true,
+            autoWidth: false
+        },
+        600: {
+            items: 3,
+            mouseDrag: false,
+            touchDrag: false
+        },
+        1000: {
+            items: 3,
+            mouseDrag: false,
+            touchDrag: false
+        }
+    }
+});
