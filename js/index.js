9da5615fd32177dcbb6f8e9bcebe5d42fb4f6eaf
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2020-03-03 18:26:04 -0300

    header sticky

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-$('document').ready(function() {
+$(document).ready(function() {
+
     $('.owl-carousel.productos').owlCarousel({
+
         margin: 0,
         responsiveClass: true,
         autoWidth: true,
+
         responsive: {
             0: {
                 items: 2,
@@ -20,12 +23,15 @@ $('document').ready(function() {
                 touchDrag: false
             }
         }
+
     });
 
     $('.owl-carousel.aceites').owlCarousel({
+
         margin: 5,
         responsiveClass: true,
         autoWidth: true,
+
         responsive: {
             0: {
                 items: 2,
@@ -44,20 +50,61 @@ $('document').ready(function() {
                 mouseDrag: false,
                 touchDrag: false
             }
+
         }
+
     });
+
+});
+
+$('#carousel').on('slide.bs.carousel', function() {
+
+    var scroll = $(window).scrollTop();
+    var hero = $('.hero');
+    var slideActive = $('#carousel .active');
+    var pageProduct = $('.producto-detalle');
+
+    if (!pageProduct && slideActive.hasClass('light') && scroll < 200) {
+
+        hero.addClass('is-dark');
+        hero.removeClass('is-light');
+
+    } else if (!pageProduct && slideActive.hasClass('dark') && scroll < 200) {
+
+        hero.addClass('is-light');
+        hero.removeClass('is-dark');
+
+    } else {
+
+        hero.addClass('is-light');
+        hero.removeClass('is-dark');
+
+    }
+
+});
+
+$(window).scroll(function() {
+
+    var header = $('.navbar');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+        header.addClass("sticky");
+    } else {
+        header.removeClass("sticky");
+    }
 
 
 });
 
-
-$('#carousel').on('slide.bs.carousel', function() {
-    var slideActive = $('#carousel .carousel-item.active');
-    if (slideActive.hasClass('light')) {
-        $('.hero').addClass('is-dark');
-        $('.hero').removeClass('is-light');
-    } else {
-        $('.hero').addClass('is-light');
-        $('.hero').removeClass('is-dark');
+$(".btn-slide-down").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        });
     }
 });
